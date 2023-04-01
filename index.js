d36188b27bd03db5d621db1949c80463b794f06b
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-04-01 22:21:17 +0300

    working on html, css and js

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputArea = document.querySelector('.inputArea');
+const btn = document.querySelector('.btn');
+const toDoList = document.querySelector('.toDoList');
+
+btn.addEventListener('click', function() {
+const item = document.createElement('li');
+item.innerText = inputArea.value;
+item.classList.add('toDoStyle');
+toDoList.appendChild(item);
+inputArea.value = '';
+
+item.addEventListener('click', function() {
+item.classList.add('toDoDone');
+})
+
+item.addEventListener('dblclick', function() {
+toDoList.removeChild(item);
+})
+
+})
